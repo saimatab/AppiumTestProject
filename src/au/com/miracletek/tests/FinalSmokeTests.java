8a34a02c4ffea494f060317e78bfd63113a581a7
saimatab <36838689+saimatab@users.noreply.github.com>
2018-04-05 09:28:11 +0500

    f
    f

--- src/au/com/miracletek/tests/FinalSmokeTests.java
+++ src/au/com/miracletek/tests/FinalSmokeTests.java
@@ -6,7 +6,8 @@ import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFRichTextString;
-  import  org.apache.poi.hssf.usermodel.DataFormatter;
+org.apache.poi.ss.usermodel.DataFormatter;
+import java.lang.Object;
 import java.util.Iterator;
 
 import java.util.*;
@@ -407,13 +408,13 @@ public class FinalSmokeTests {
 		HSSFWorkbook	  workbook2 = new HSSFWorkbook(fileInputStream2);
        HSSFSheet sheet1 = workbook1.getSheetAt(0);
       HSSFSheet sheet2 = workbook2.getSheetAt(0);
-        Iterator<Row> rowIterator1 = sheet1.iterator();
-        Iterator<Row> rowIterator2 = sheet2.iterator();
+        Iterator<HSSFRow> rowIterator1 = sheet1.iterator();
+        Iterator<HSSFRow> rowIterator2 = sheet2.iterator();
         while (rowIterator1.hasNext() && rowIterator2.hasNext()) {
             HSSFRow currentRow1 = rowIterator1.next();
             HSSFRow currentRow2 = rowIterator1.next();
-            Iterator<Cell> cellIterartor1 = currentRow1.iterator();
-            Iterator<Cell> cellIterator2 = currentRow2.iterator();
+            Iterator<HSSFCell> cellIterartor1 = currentRow1.iterator();
+            Iterator<HSSFCell> cellIterator2 = currentRow2.iterator();
             while (cellIterartor1.hasNext() && cellIterator2.hasnext()) {
                 HSSFCell currentCell1 = cellIterartor1.next();
                 HSSFCell currentCell2 = cellIterartor2.next();
