bc42c8290ceb3b86dc4d5ea8f646cb4aa60aa390
saimatab <36838689+saimatab@users.noreply.github.com>
2018-04-05 15:17:37 +0500

    h
    g

--- src/au/com/miracletek/tests/FinalSmokeTests.java
+++ src/au/com/miracletek/tests/FinalSmokeTests.java
@@ -483,7 +483,7 @@ public class FinalSmokeTests {
 			}
 		}
 				
-			workbook1.write(new FileOutputStream("auto_output_results.xls"));
+			workbook1.write(new FileOutputStream("auto_output_results_final.xls"));
 	//workbook2.write(new FileOutputStream("auto_input.xls"));
 
 			
