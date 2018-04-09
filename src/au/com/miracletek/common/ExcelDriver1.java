4850c996aa786982007ca315fce46daa40e681b2
saima tabassum <saimatab2016@gmail.com>
2018-04-09 09:46:12 +0500

    g
    g

--- src/au/com/miracletek/common/ExcelDriver1.java
+++ src/au/com/miracletek/common/ExcelDriver1.java
@@ -253,7 +253,7 @@ ResultSetMetaData colInfo1 = rs1.getMetaData();
   System.out.println("%%%%%%%%%%%%@@@@@@@########@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@^^^^^^^^^^^^@%%%%%PPP24235235356346PP1");
   
 	  for (String colName : colNames1) {
-			System.out.println("%%%%%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@^^^^^^^^^^^^@%%%%%PPP24235235356346PP1");
+			System.out.println("%%%%%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@^^^^^^^^^^^^@%%%%%PPP24235235356346PP1"+colname);
 		    dataRow4.createCell(0).setCellValue(
 		      new XSSFRichTextString(rs1.getString(colName)));
 		      
