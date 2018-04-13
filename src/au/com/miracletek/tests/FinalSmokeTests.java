0528aab598a7d05e591e7127f68a87aa6219fb7f
saima tabassum <saimatab2016@gmail.com>
2018-04-13 13:42:15 +0500

    f
    f

--- src/au/com/miracletek/tests/FinalSmokeTests.java
+++ src/au/com/miracletek/tests/FinalSmokeTests.java
@@ -421,9 +421,10 @@ public class FinalSmokeTests {
 									    driver3.findElement(By.id("Password")).sendKeys("Goodluck1@");
 									    driver3.findElement(By.cssSelector("button.btn.btn-primary")).click();
 										WebDriverWait wait = new WebDriverWait(driver3, 300);
-							         	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='item_Tasks']/span"))).click();
-								
-									    driver3.findElement(By.id("createNewButton")).click();
+							         	WebElement h=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='item_Tasks']/span")));
+							         	 driver3.findElement(By.xpath(".//*[@id='item_Tasks']/span")).click();
+									    driver3.findElement(By.id("createNewButton")).click();									    Thread.sleep(3000);
+
 									    Thread.sleep(1000);
 									 //  driver3.findElement(By.id("taskName")).clear();
 									    driver3.findElement(By.id("taskName")).sendKeys("simple1");
